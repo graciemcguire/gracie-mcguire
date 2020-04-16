705a750fbd7fc925e9c5d06d620da9ea345bb05e
Gracie <m.graciemcguire@gmail.com>
2020-04-16 10:15:54 -0600

    begin combine portfolios

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import github from './github.svg'
+import linkedin from './linkedin.png'
 
 function App() {
+  console.log("Made with ❤️ by Gracie McGuire in 2019");
+  console.log("github.com/graciemcguire 🏝");
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <div className='main'>
+        <h1>Gracie McGuire</h1>
+        <p>Software Engineer | Designer</p>
+
+        <div className= 'icon-links'>
+        <a href='https://github.com/graciemcguire' target="_blank" rel="noopener noreferrer" >
+          <img className='icon' src={ github } alt='github' />
         </a>
-      </header>
-    </div>
+
+        <a href='https://www.linkedin.com/in/gracie-mcguire-43a9b361/' target="_blank" rel="noopener noreferrer" >
+          <img className='icon' src={ linkedin } alt='linkedin' />
+        </a>
+        </div>
+
+      </div>
   );
 }
 
