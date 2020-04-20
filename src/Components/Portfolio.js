5b86dbae49331e4ace997e76ae7f2d408720f029
Gracie <m.graciemcguire@gmail.com>
2020-04-20 13:43:52 -0600

    add CNAME file and modify package

--- src/Components/Portfolio.js
+++ src/Components/Portfolio.js
@@ -8,16 +8,21 @@ function Portfolio (props) {
       <div className='icon-links'>
         { renderContact() }
       </div>
-      <h1>Portfolio</h1>
-      <div className='render-modal'>
-        <div className='portfolio btn'>Web Development</div>
-        <div className='portfolio btn'>Design</div>
-        <div className='portfolio btn'>Everything</div>
-      </div>
-      <div className='buttons port'>
-        <Link to='/'> <button className='btn'>Home</button> </Link>
-        <Link to='/about'> <button className='btn'>About</button> </Link>
-      </div>
+
+      <div className='main'>
+
+        <h1>Portfolio</h1>
+        <div className='render-modal'>
+          <div className='portfolio btn'>Web Development</div>
+          <div className='portfolio btn'>Design</div>
+          <div className='portfolio btn'>Everything</div>
+        </div>
+        <div className='buttons port'>
+          <Link to='/'> <button className='btn'>Home</button> </Link>
+          <Link to='/about'> <button className='btn'>About</button> </Link>
+        </div>
+
+        </div>
 
     </div>
   )
