bc72854c728d316c04ae0bc60929df28c8e432e3
Gracie <m.graciemcguire@gmail.com>
2020-04-20 15:19:55 -0600

    adds email icon

--- src/Components/contact.js
+++ src/Components/contact.js
@@ -6,7 +6,7 @@ const mapContact = () => {
   return contact.map(site => {
     return (
       <a className='icon' href= { site.url } target="_blank" rel="noopener noreferrer">
-        <i className={ site.icon }></i>
+        <i alt={ site.name } className={ site.icon }></i>
       </a>
     )
   })
@@ -16,8 +16,9 @@ const renderContact = () => {
   return (
     <div className='icon-links'>
       <a className='icon' href= { resume } target="_blank" rel="noopener noreferrer">
-        <i className="fa fa-sticky-note"></i>
+        <i alt='resume' className="fa fa-sticky-note"></i>
       </a>
+       
       { mapContact() }
     </div>
   )
