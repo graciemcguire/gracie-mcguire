1148284e1d1c38a9ad25dfba22b5d038bf25a4c7
Gracie <m.graciemcguire@gmail.com>
2020-04-20 12:56:13 -0600

    fix links and buttons

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,8 +5,6 @@ import { Link } from 'react-router-dom'
 
 function About (props) {
 
-
-
   return(
     <div>
 
@@ -17,13 +15,13 @@ function About (props) {
       <div className='main'>
         <h1>Hello!</h1>
         <div className='bio'>
-        I am a full stack software engineer and designer born and raised in Denver, but currently residing in Brooklyn. I have a passion for social justice and progessive politics, and want to use my skills and my voice to help make the world a better and more inclusive place. I love finding creative solutions to problems, and am always striving to learn more. 
+          I am a full stack software engineer and designer born and raised in Denver, but currently residing in Brooklyn. I have a passion for social justice and progessive politics, and want to use my skills and my voice to help make the world a better and more inclusive place. I love finding creative solutions to problems, and am always striving to learn more.
         </div>
 
 
         <div className='buttons'>
-          <Link to='/'> <button>Home</button> </Link>
-          <Link to='/portfolio'> <button>Portfolio</button> </Link>
+          <Link to='/'> <button className='btn'>Home</button> </Link>
+          <Link to='/portfolio'> <button className='btn'>Portfolio</button> </Link>
         </div>
 
       </div>
@@ -32,4 +30,4 @@ function About (props) {
     </div>
   )
 }
-export default About
+export default About;
---
+++ src/Components/Portfolio.js
@@ -0,0 +1,25 @@
+import React from 'react'
+import renderContact from './contact'
+import { Link } from 'react-router-dom'
+
+function Portfolio (props) {
+  return(
+    <div>
+      <div className='icon-links'>
+        { renderContact() }
+      </div>
+      <h1>Portfolio</h1>
+      <div className='render-modal'>
+        <div className='portfolio btn'>Web Development</div>
+        <div className='portfolio btn'>Design</div>
+        <div className='portfolio btn'>Everything</div>
+      </div>
+      <div className='buttons port'>
+        <Link to='/'> <button className='btn'>Home</button> </Link>
+        <Link to='/about'> <button className='btn'>About</button> </Link>
+      </div>
+
+    </div>
+  )
+}
+export default Portfolio
